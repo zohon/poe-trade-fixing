8d9fcd1d32bbbfee35e127f55ba148cd6c6e86fa
Edouard ERY. RABANY <erabany@piksel.int>
2017-08-24 12:01:07 +0200

    Signed-off-by: Edouard ERY. RABANY <erabany@piksel.int>

--- main.js
+++ main.js
@@ -159,15 +159,12 @@ chrome.storage.local.get('myExtensionId', function(result) {
   }
 });
 
-
-
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
 }
-
 
 // chrome.storage.local.set({
 //   'myKarma': myKarma
